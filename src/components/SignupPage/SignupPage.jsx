b03e02704fc719d6d51e7e97341d0c8847035165
Aparna1567 <aparnaapar99@gmail.com>
2026-06-05 15:52:29 +0530

    removed font

--- src/components/SignupPage/SignupPage.jsx
+++ src/components/SignupPage/SignupPage.jsx
@@ -255,13 +255,13 @@ const SignupPage = () => {
         }}
       />
 
-      {/* Font Import */}
+      {/* Font Import
       <style>
         {`
           @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap');
           body { font-family: 'Montserrat', sans-serif; }
         `}
-      </style>
+      </style> */}
     </div>
   );
 };
